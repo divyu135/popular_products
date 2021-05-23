88df2c56c33e396d96f35ff5e74f91b4d1693074
Divyajeet Chauhan <divyajeetchauhan@gmail.com>
2021-05-23 18:42:24 -0400

    refractoring components to avoid bind

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,15 @@
 class ProductList extends React.Component {
 
-    constructor(props) {
-        super(props);
+    state = {
+        products : [],
+    };
 
-        this.state = {
-            products : [],
-        };
-
-        this.handleProductUpVote = this.handleProductUpVote.bind(this);
-    }
 
     componentDidMount() {
         this.setState({products:Seed.products});
     }
 
-    handleProductUpVote(productId) {
+    handleProductUpVote = (productId) => {
         const nextProducts = this.state.products.map( (product) => {
             if (product.id === productId) {
                 return Object.assign({},product,{
@@ -59,14 +54,10 @@ class ProductList extends React.Component {
 
 class Product extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.handleUpVote = this.handleUpVote.bind(this);
-    }
 
-    handleUpVote() {
-        this.props.onVote(this.props.id);
-    }
+    handleUpVote = ()=> (
+        this.props.onVote(this.props.id)
+    );
     render() {
         return (
             <div className='item'>
