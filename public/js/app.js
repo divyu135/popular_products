e77e0c2552dcfb34527873060d9347d739836ba9
Divyajeet Chauhan <divyajeetchauhan@gmail.com>
2021-05-22 21:47:45 -0400

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, friend! I am a basic React Component
+            </div>
+        )
+    }
+}
