0c2a6e7d02ad896d9f0bab6b0d62c2a306c6fb21
Divyajeet Chauhan <divyajeetchauhan@gmail.com>
2021-05-23 01:03:05 -0400

    Create components product and product list

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,38 @@
 class ProductList extends React.Component {
     render() {
         return (
-            <div>
-                Hello, friend! I am a basic React Component
+            <div className="ui unstackable items">
+                <Product />
             </div>
-        )
+        );
     }
 }
+
+class Product extends React.Component {
+    render() {
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src ='images/products/image-aqua.png' />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='description'>
+                        <a>Fort Knight</a>
+                        <p>A set of Cosmetics in Battle Royale, delivered in just two weeks.</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img 
+                            className='ui avatar image' 
+                            src = 'images/avatars/daniel.jpg' />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+)
